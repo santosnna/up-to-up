9ed7a038d923cc7c559c17740dc1955e1b813d0f
santosnna <nathan.santos01@fatec.sp.gov.br>
2020-10-14 16:04:49 -0300

    Roll back to test heroku deployment without database connection

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const AppsService = require('./services/AppsService');
+const AppsService = require("./services/AppsService");
 
-const appsService = new AppsService('./data/apps.json');
+const appsService = new AppsService("./data/apps.json");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
 
-const port = 3002;
-// const port = 3000;
+let port = process.env.PORT;
+if (port == null || port === "") {
+  port = 3002;
+}
+// const port = 3002;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
 
-app.use(express.static(path.join(__dirname, './static')));
+app.use(express.static(path.join(__dirname, "./static")));
 
-app.locals.pageTitle = 'Up-to-Up';
-app.locals.pageSlogan = 'Renda extra com diversão!';
+app.locals.pageTitle = "Up-to-Up";
+app.locals.pageSlogan = "Renda extra com diversão!";
 
-
-app.use('/', routes({
-  appsService
-}));
+app.use(
+  "/",
+  routes({
+    appsService,
+  })
+);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
